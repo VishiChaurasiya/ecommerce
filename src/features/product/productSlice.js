44ecc11413a2a6fedcf490b46730aec2a6109707
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-02-24 07:49:29 +0530

    Product page changes and filters

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,13 +1,14 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProducts, fetchProductsByFilters } from "./productAPI";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchAllProducts,fetchProductsByFilters } from './productAPI';
 
 const initialState = {
   products: [],
-  status: "idle",
+  status: 'idle',
 };
 
+
 export const fetchAllProductsAsync = createAsyncThunk(
-  "product/fetchAllProducts",
+  'product/fetchAllProducts',
   async () => {
     const response = await fetchAllProducts();
     // The value we return becomes the `fulfilled` action payload
@@ -15,16 +16,18 @@ export const fetchAllProductsAsync = createAsyncThunk(
   }
 );
 export const fetchProductsByFiltersAsync = createAsyncThunk(
-  "product/fetchProductsByFilters",
-  async (filter) => {
-    const response = await fetchProductsByFilters(filter);
+  'product/fetchProductsByFilters',
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilters(filter,sort);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
+
+
 export const productSlice = createSlice({
-  name: "product",
+  name: 'product',
   initialState,
   reducers: {
     increment: (state) => {
@@ -34,17 +37,17 @@ export const productSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchAllProductsAsync.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.status = 'idle';
         state.products = action.payload;
       })
       .addCase(fetchProductsByFiltersAsync.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.status = 'idle';
         state.products = action.payload;
       });
   },
